216a88c1e9470841f30145a7049bdf87bc9e6f57
Kousik Rajesh <kousik18@iitg.ac.in>
2020-03-11 01:25:47 +0530

    odd/even implementation

--- data-gen.py
+++ data-gen.py
@@ -1,10 +1,16 @@
 import random
 import numpy as np
 import pickle
-xtrain=np.random.randint(2,size=(100,8))
+xtrain=np.random.randint(2,size=(9000,64))
 ytrain=xtrain[:,-1]
-
-np.save('xtest',xtrain)
-np.save('ytest',ytrain)
+y=[]
+for i in range(9000):
+	if(ytrain[i]==1):
+		y.append(0)
+	else:
+		y.append(1)
+ytrain=np.array(y)
+np.save('xtrain',xtrain)
+np.save('ytrain',ytrain)
 print(xtrain)
 print(ytrain)
