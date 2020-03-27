5988e3eb4cd1df02f5f5aa25c9810ce8b12244ac
Kousik Rajesh <kousik18@iitg.ac.in>
2020-03-27 16:46:39 +0530

    Completed tasks

--- data-gen.py
+++ data-gen.py
@@ -1,10 +1,11 @@
 import random
 import numpy as np
 import pickle
-xtrain=np.random.randint(2,size=(9000,64))
+num_samples=25000
+xtrain=np.random.randint(2,size=(num_samples,256))
 ytrain=xtrain[:,-1]
 y=[]
-for i in range(9000):
+for i in range(num_samples):
 	if(ytrain[i]==1):
 		y.append(0)
 	else:
